77bc803d695b56cb88ca9e84e00d2c686b2b371e
Ugurismail <ugurismail@Ugur-MacBook-Air.local>
2026-05-29 14:59:32 +0300

    Merge core migration branches

---
+++ core/migrations/0055_merge_0054_attendance_online_chat.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0054_attendancesheetconfig_attendancedaystate'),
+        ('core', '0054_userprofile_online_chat_last_read_at'),
+    ]
+
+    operations = []
